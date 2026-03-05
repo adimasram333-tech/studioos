2e3e40cbbeaae9c9217301816ebe0dbde05b692b
adimasram333-tech <adimasram333@gmail.com>
2026-03-06 00:29:09 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -3,41 +3,47 @@ const SUPABASE_URL = "https://gnnaaagvlrmdveqxicob.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_TnjoiedXWPbSjjqh2tmfsQ_kpiIMaND";
 
 const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+SUPABASE_URL,
+SUPABASE_ANON_KEY
 );
 
-async function saveQuotation(data) {
+async function saveQuotation(data){
 
-  const { data: result, error } = await supabase
-    .from("quotations")
-    .insert([data])
-    .select();
+const {data:result,error}=await supabase
+.from("quotations")
+.insert([data])
+.select();
 
-  if (error) {
-    console.error(error);
-    alert("Error saving quotation");
-    return null;
-  }
+if(error){
 
-  return result[0];
+console.error(error);
+alert("Error saving quotation");
+return null;
+
 }
 
-async function getQuotationById(id) {
+return result[0];
 
-  const { data, error } = await supabase
-    .from("quotations")
-    .select("*")
-    .eq("id", id)
-    .single();
-
-  if (error) {
-    console.error(error);
-    return null;
-  }
-
-  return data;
 }
 
-window.saveQuotation = saveQuotation;
-window.getQuotationById = getQuotationById;
+async function getQuotationById(id){
+
+const {data,error}=await supabase
+.from("quotations")
+.select("*")
+.eq("id",id)
+.single();
+
+if(error){
+
+console.error(error);
+return null;
+
+}
+
+return data;
+
+}
+
+window.saveQuotation=saveQuotation;
+window.getQuotationById=getQuotationById;
