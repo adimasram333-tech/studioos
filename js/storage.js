b3c0bec14bbc52a684e1977928bb503a70df615b
adimasram333-tech <adimasram333@gmail.com>
2026-03-06 01:23:51 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,4 @@
 const SUPABASE_URL = "https://gnnaaagvlrmdveqxicob.supabase.co";
-
 const SUPABASE_ANON_KEY = "sb_publishable_TnjoiedXWPbSjjqh2tmfsQ_kpiIMaND";
 
 const supabase = window.supabase.createClient(
@@ -9,17 +8,15 @@ SUPABASE_ANON_KEY
 
 async function saveQuotation(data){
 
-const {data:result,error}=await supabase
+const { data:result, error } = await supabase
 .from("quotations")
 .insert([data])
 .select();
 
 if(error){
-
 console.error(error);
 alert("Error saving quotation");
 return null;
-
 }
 
 return result[0];
@@ -28,22 +25,20 @@ return result[0];
 
 async function getQuotationById(id){
 
-const {data,error}=await supabase
+const { data, error } = await supabase
 .from("quotations")
 .select("*")
-.eq("id",id)
+.eq("id", id)
 .single();
 
 if(error){
-
 console.error(error);
 return null;
-
 }
 
 return data;
 
 }
 
-window.saveQuotation=saveQuotation;
-window.getQuotationById=getQuotationById;
+window.saveQuotation = saveQuotation;
+window.getQuotationById = getQuotationById;
