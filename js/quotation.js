c0274ae53a1e9057f7f72e57df047659fccda461
adimasram333-tech <adimasram333@gmail.com>
2026-03-06 16:16:07 +0530

    Update quotation.js

--- js/quotation.js
+++ js/quotation.js
@@ -1,7 +1,21 @@
 const packageSelect = document.getElementById("packageSelect")
+
 const totalInput = document.getElementById("totalAmount")
+
 const advanceInput = document.getElementById("advanceAmount")
+
 const balanceInput = document.getElementById("balanceAmount")
+
+const albumCheck = document.getElementById("albumCheck")
+
+const albumPages = document.getElementById("albumPages")
+
+const giftCheck = document.getElementById("giftCheck")
+
+const giftName = document.getElementById("giftName")
+
+
+// PACKAGE PRICE AUTO
 
 packageSelect.addEventListener("change",function(){
 
@@ -10,6 +24,9 @@ totalInput.value = this.value
 calculateBalance()
 
 })
+
+
+// BALANCE CALC
 
 advanceInput.addEventListener("input",calculateBalance)
 
@@ -21,6 +38,43 @@ const advance = parseFloat(advanceInput.value) || 0
 balanceInput.value = total - advance
 
 }
+
+
+// ALBUM INPUT SHOW
+
+albumCheck.addEventListener("change",function(){
+
+if(this.checked){
+
+albumPages.classList.remove("hidden")
+
+}else{
+
+albumPages.classList.add("hidden")
+
+}
+
+})
+
+
+// GIFT INPUT SHOW
+
+giftCheck.addEventListener("change",function(){
+
+if(this.checked){
+
+giftName.classList.remove("hidden")
+
+}else{
+
+giftName.classList.add("hidden")
+
+}
+
+})
+
+
+// PREVIEW
 
 document.getElementById("previewBtn").addEventListener("click",function(){
 
