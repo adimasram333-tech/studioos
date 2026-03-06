b65991879d750f2a556924eecf291166a0b58046
adimasram333-tech <adimasram333@gmail.com>
2026-03-06 17:34:14 +0530

    Update proposal.js

--- js/proposal.js
+++ js/proposal.js
@@ -2,32 +2,46 @@ const data = JSON.parse(localStorage.getItem("quotationData"))
 
 if(data){
 
+// CLIENT
+
 document.getElementById("clientName").innerText =
 data.clientName || "-"
 
 
-// EVENT DATE RANGE
+// EVENT DATE
 
-const dateText =
+document.getElementById("eventDate").innerText =
+(data.startDate || "-") + " to " + (data.endDate || "-")
 
-(data.startDate || "-") +
-"  to  " +
-(data.endDate || "-")
 
-document.getElementById("eventDate").innerText = dateText
-
+// MONEY FORMAT
 
 document.getElementById("total").innerText =
-data.total || "-"
+"₹ " + (data.total || "0") + " /-"
 
 document.getElementById("advance").innerText =
-data.advance || "-"
+"₹ " + (data.advance || "0") + " /-"
 
 document.getElementById("balance").innerText =
-data.balance || "-"
+"₹ " + (data.balance || "0") + " /-"
+
+
+// DELIVERABLES
+
+const list = document.getElementById("deliverablesList")
+list.innerHTML = ""
+
+if(data.raw) list.innerHTML += "<li>All Raw Soft Copy</li>"
+
+if(data.traditional) list.innerHTML += "<li>Traditional Full Event Video</li>"
+
+if(data.cinematic) list.innerHTML += "<li>Cinematic Highlight Film</li>"
+
+if(data.album) list.innerHTML += "<li>Premium Printed Album ("+data.albumPages+" pages)</li>"
+
+if(data.gift) list.innerHTML += "<li>Complimentary Gift : "+data.giftName+"</li>"
 
 }
-
 
 
 // STUDIO NAME
