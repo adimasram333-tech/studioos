276974b5cbb496e732cc981aae701906628f2d09
adimasram333-tech <adimasram333@gmail.com>
2026-03-15 18:43:38 +0530

    Create cloudinary.js

---
+++ js/cloudinary.js
@@ -0,0 +1,22 @@
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/YOUR_CLOUD_NAME/image/upload"
+
+const UPLOAD_PRESET = "studioos_gallery"
+
+async function uploadToCloudinary(file,eventId){
+
+const formData = new FormData()
+
+formData.append("file",file)
+formData.append("upload_preset",UPLOAD_PRESET)
+formData.append("folder","studioos/"+eventId)
+
+const res = await fetch(CLOUDINARY_URL,{
+method:"POST",
+body:formData
+})
+
+const data = await res.json()
+
+return data.secure_url
+
+}
