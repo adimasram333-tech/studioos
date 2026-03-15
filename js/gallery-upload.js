4fa285224aa711df3d0baee553433325ce884ab5
adimasram333-tech <adimasram333@gmail.com>
2026-03-15 18:46:22 +0530

    Update gallery-upload.js

--- js/gallery-upload.js
+++ js/gallery-upload.js
@@ -11,16 +11,27 @@ status.innerText = "Please select images or folder"
 return
 }
 
-status.innerText = "Uploading..."
+status.innerText = "Preparing upload..."
 
 let uploaded = 0
 
+// event id create or get
+let eventId = localStorage.getItem("current_event")
+
+if(!eventId){
+eventId = crypto.randomUUID()
+localStorage.setItem("current_event",eventId)
+}
+
 const uploadFile = async (file) => {
+
+try{
 
 let formData = new FormData()
 
 formData.append("file", file)
 formData.append("upload_preset", UPLOAD_PRESET)
+formData.append("folder","studioos/"+eventId)
 
 const res = await fetch(
 `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
@@ -36,6 +47,15 @@ uploaded++
 status.innerText = `Uploaded ${uploaded} / ${files.length}`
 
 return data.secure_url
+
+}catch(err){
+
+console.error("Upload error:",err)
+
+status.innerText = "Upload error occurred"
+
+}
+
 }
 
 const uploads = []
@@ -46,7 +66,11 @@ uploads.push(uploadFile(file))
 
 }
 
-await Promise.all(uploads)
+// parallel upload
+const uploadedUrls = await Promise.all(uploads)
+
+console.log("Uploaded Images:",uploadedUrls)
 
 status.innerText = "Upload Complete"
+
 }
