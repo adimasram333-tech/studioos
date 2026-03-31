5cfc07da3e42d8e72f9ff11a5e310f864b062921
adimasram333-tech <adimasram333@gmail.com>
2026-03-31 20:04:17 +0530

    Update gallery-delete.js

--- js/gallery-delete.js
+++ js/gallery-delete.js
@@ -2,7 +2,7 @@
 // DELETE MODULE (FINAL CLEAN)
 // =============================
 
-window.deleteEvent = async function(eventId){
+window.deleteGallery = async function(eventId){
 
 const confirmDelete = confirm("Delete gallery permanently?")
 if(!confirmDelete) return
@@ -14,8 +14,8 @@ const res = await fetch("https://gnnaaagvlrmdveqxicob.supabase.co/functions/v1/s
 method: "POST",
 headers: {
 "Content-Type": "application/json",
-"Authorization": "Bearer YOUR_SUPABASE_ANON_KEY",
-"apikey": "YOUR_SUPABASE_ANON_KEY"
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdubmFhYWd2bHJtZHZlcXhpY29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0OTk4NTQsImV4cCI6MjA4ODA3NTg1NH0.LgK0WDOa1wp4vhUS3BjvQUpvU_pENGTZegbCtd_HWNE",
+"apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdubmFhYWd2bHJtZHZlcXhpY29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0OTk4NTQsImV4cCI6MjA4ODA3NTg1NH0.LgK0WDOa1wp4vhUS3BjvQUpvU_pENGTZegbCtd_HWNE"
 },
 body: JSON.stringify({ event_id: eventId })
 })
